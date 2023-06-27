e45e3dab7339ce475a53ca700ee8cc443844e6e4
rio.loann <rio.loann@gmail.com>
2023-06-27 22:02:59 +0200

    clean code

--- flake.py
+++ flake.py
@@ -1,12 +1,12 @@
-# flake
-
 import random
 import pygame
 
 pygame.init()
 
-listOfPoint = [(450.0, 250.0), (350.0, 423.2050807568877), (150.00000000000006, 423.20508075688775), (50.0, 250.00000000000003), (149.99999999999991, 76.7949192431123), (350.0, 76.79491924311228)]
+# vertex of the hexagone
+listOfPoint = [(450.0, 250.0), (350.0, 423.20), (150, 423.20), (50.0, 250), (150, 76.79), (350.0, 76.79)]
 
+# init screen
 screen = pygame.display.set_mode((500, 500))
 screen.fill((250, 250, 250))
 pygame.draw.polygon(screen, (0, 0, 0), listOfPoint, 1)
@@ -14,20 +14,21 @@ pygame.draw.polygon(screen, (0, 0, 0), listOfPoint, 1)
 newPointX = 200
 newPointY = 200
 
-done = False
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-    
+for i in range(60000):
+
+    # chose a random vertex
     corner = random.randrange(6)
 
+    # get vector btw current point and vertex
     vectX = listOfPoint[corner][0] - newPointX
     vectY = listOfPoint[corner][1] - newPointY
 
+    # get new point
     newPointX = newPointX + vectX*2/3
     newPointY = newPointY + vectY*2/3
 
+    # draw new point
     pygame.draw.circle(screen, (255, 0, 0), (newPointX, newPointY), 1)
 
+    # draw
     pygame.display.flip()
